50e502b7b1227115f870d4d89b5779f263f3c99d
Pabl0FPP <pinedapablo6718@gmail.com>
2025-10-30 22:05:02 -0500

    feat: update order service

--- order-service/src/main/java/com/selimhorri/app/helper/CartMappingHelper.java
+++ order-service/src/main/java/com/selimhorri/app/helper/CartMappingHelper.java
@@ -23,11 +23,7 @@ public interface CartMappingHelper {
 				.userId(cartDto.getUserId())
 				.build();
 	}
-	
-	
-	
 }
-
 
 
 
--- order-service/src/main/java/com/selimhorri/app/repository/OrderRepository.java
+++ order-service/src/main/java/com/selimhorri/app/repository/OrderRepository.java
@@ -1,11 +1,15 @@
 package com.selimhorri.app.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.selimhorri.app.domain.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-	
-	
-	
+
+    List<Order> findAllByIsActiveTrue();
+
+    Optional<Order> findByOrderIdAndIsActiveTrue(Integer orderId);
 }
