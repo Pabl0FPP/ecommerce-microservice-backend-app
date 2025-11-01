b913e9d5cfad9820f22e3d2709ad0df9419a4a3a
Pabl0FPP <pinedapablo6718@gmail.com>
2025-10-31 20:51:13 -0500

    feat: update sql file of product service

--- product-service/src/main/resources/db/migration/V4__insert_products_table.sql
+++ product-service/src/main/resources/db/migration/V4__insert_products_table.sql
@@ -1,8 +1,8 @@
 
 INSERT INTO products
 (category_id, product_title, image_url, sku, price_unit, quantity) VALUES
-(1, 'asus', 'xxx', 'dfqejklejrkn', 0, 50),
-(1, 'hp', 'xxx', 'zsejfedbjh', 0, 50),
+(3, 'asus', 'xxx', 'dfqejklejrkn', 0, 50),
+(3, 'hp', 'xxx', 'zsejfedbjh', 0, 50),
 (2, 'Armani', 'xxx', 'fjdvf', 0, 50),
-(3, 'GTA', 'xxx', 'qsdkjnvfrekjrf', 0, 50);
+(5, 'GTA', 'xxx', 'qsdkjnvfrekjrf', 0, 50);
 
